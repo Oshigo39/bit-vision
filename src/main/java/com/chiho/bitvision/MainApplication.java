a7f00ba6b8f051d75e75705b8b2054dbc0adfe8a
Oshigo <1950519496@qq.com>
2025-11-19 20:12:09 +0800

    Init

---
+++ src/main/java/com/chiho/bitvision/MainApplication.java
@@ -0,0 +1,28 @@
+package com.chiho.bitvision;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 主类（项目启动入口）
+ *
+ */
+@EnableAsync
+@EnableScheduling
+@EnableTransactionManagement
+@SpringBootApplication
+@MapperScan("com.chiho.bitvision.mapper")
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
